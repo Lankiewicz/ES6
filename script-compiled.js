7f2a687f7b7992558f176b38062e7dda6b6e644e
Jakub Lankiewicz <lankiewiczjakub@gmail.com>
2018-04-23 20:51:34 +0200

    es6

--- script-compiled.js
+++ script-compiled.js
@@ -13,8 +13,22 @@ function Multiply() {
 };
 Multiply();
 
+//zadanie 3
 function add() {
-const add = (a,b) => a+b;
-console.log(add());
+const add = (a,b,c,d) => (a+b+c+d)/4;	
+console.log(add(1,3,6,6));
 }
-add(1,3);
+add();
+
+
+//zadanie 4
+
+function checkParams(...params) {
+    const grades = [1, 5, 5, 5, 4, 3, 3, 2, 1]
+    console.log(grades); 
+}
+checkParams();
+//zadanie 5
+var array = [1, 4, 'Iwona', false, 'Nowak'];
+const [ , ,third, , fifth] = array;
+console.log(third, fifth);
