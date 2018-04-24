c238eb0126a14f66071dca559bfddd7612a69d80
Jakub Lankiewicz <lankiewiczjakub@gmail.com>
2018-04-24 18:07:34 +0200

    update

--- js/script.js
+++ js/script.js
@@ -1,14 +1,34 @@
 //Zadanie 1
-function sayHelloTo (){
+function sayHelloTo() {
 	const Hello = 'Hello';
 	const World = 'World';
-    console.log(`${Hello} ${World}`);
+	console.log(`${Hello} ${World}`);
 };
 sayHelloTo();
 //zadanie 2
-function Multiply (){
-	const numbers = [1,2];
-	const numbersPlusOne = numbers.map( x => x * 1 );
-	console.log(`${numbersPlusOne}`);
+function Multiply() {
+	const a = [2];
+	const numbers = a.map( x=> {return x + 1});
+	console.log(`${numbers}`);
 };
 Multiply();
+
+//zadanie 3
+function add() {
+const add = (a,b,c,d) => (a+b+c+d)/4;	
+console.log(add(1,3,6,6));
+}
+add();
+
+
+//zadanie 4
+
+function checkParams(...params) {
+    const grades = [1, 5, 5, 5, 4, 3, 3, 2, 1]
+    console.log(grades); 
+}
+checkParams();
+//zadanie 5
+var array = [1, 4, 'Iwona', false, 'Nowak'];
+const [ , ,third, , fifth] = array;
+console.log(third, fifth);
